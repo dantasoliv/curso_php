7805c6bb41e22e1814468200a686e38fce1c11b4
Daniel Dantas <danieldantasoliv@gmail.com>
2021-08-17 21:30:50 -0300

    Aula Funções - Closure e Callabe

---
+++ funcoes/closure_callable.php
@@ -0,0 +1,22 @@
+<div class="titulo">Closure & Callable</div>
+
+<?php
+
+$soma1 = function($a, $b) {
+    return $a + $b;
+};
+
+echo $soma1(2, 3) . '<br>';
+echo (is_callable($soma1) ? 'Sim' : 'Não') . '<br>';
+
+//$soma1 = 1;
+//echo (is_callable($soma1) ? 'Sim' : 'Não') . '<br>';
+
+function soma2($a, $b) {
+    return $a + $b;
+}
+
+echo soma2(2, 3) . '<br>';
+echo (is_callable(soma2) ? 'Sim' : 'Não') . '<br>';
+
+var_dump($soma1);
