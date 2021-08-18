5a093c437a41b60bb770a852b4d285b456337c03
Daniel Dantas <danieldantasoliv@gmail.com>
2021-08-17 23:14:26 -0300

    Aula Funções - Retornando Funções

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
                     <li><a href="exercicio.php?dir=funcoes&file=closure_callable">Closure & Callable</a></li>
                     <li><a href="exercicio.php?dir=funcoes&file=recursividade">Recursividade</a></li>
                     <li><a href="exercicio.php?dir=funcoes&file=desafio_recursao">Desafio Recursão</a></li>
+                    <li><a href="exercicio.php?dir=funcoes&file=retornando_funcao">Retornando Função</a></li>
                     </ul>
             </div>
             <div class="modulo verde-escuro">
