fc7c040afd58f0f47ce0716a6de9348b4c902c9c
Daniel Dantas <danieldantasoliv@gmail.com>
2021-10-12 17:56:57 -0300

    Aula Tratamento de Erros - Desafio intdiv

--- template/menu.php
+++ template/menu.php
@@ -4,6 +4,7 @@
                     <ul>
                         <li><a href="exercicio.php?dir=tratamento_erro&file=tray_catch">Tray/Catch</a></li>
                         <li><a href="exercicio.php?dir=tratamento_erro&file=erros_personalizados">Erros Personalizados</a></li>
+                        <li><a href="exercicio.php?dir=tratamento_erro&file=desafio_intdiv_teste">Desafio Intdiv</a></li>
                     </ul>
                 </div>
                 <div class="modulo laranja-escuro">
