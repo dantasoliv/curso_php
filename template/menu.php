3d302f7b60daa7420a062771548fbf82c43d3aa5
Daniel Dantas <danieldantasoliv@gmail.com>
2021-11-02 15:46:12 -0300

    Aula API - Desafio Imagens

--- template/menu.php
+++ template/menu.php
@@ -8,6 +8,7 @@
                         <li><a href="exercicio.php?dir=api&file=ler_arquivo">Ler Arquivo</a></li>
                         <li><a href="exercicio.php?dir=api&file=upload">Upload</a></li>
                         <li><a href="exercicio.php?dir=api&file=download">Download</a></li>
+                        <li><a href="exercicio.php?dir=api&file=imagens">Imagens</a></li>
                     </ul>
                 </div>
                 <div class="modulo vermelho">
